a37018779d18ba64888fc5eacb86c23f7a20226d
Ludovic Rivallain <ludovic.rivallain@gmail.com>
2022-10-13 12:17:19 +0200

    Simplify

---
+++ app.py
@@ -0,0 +1,37 @@
+"""Main module."""
+
+import random
+from flask import Flask, render_template, make_response
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+def get_quote():
+    """Return a random quote from data
+
+    Returns:
+        str: a random quote
+    """
+    with open("data/quotes.txt", "r", encoding="utf-8") as file:
+        quotes = file.readlines()
+    return random.choice(quotes)
+
+@app.route('/')
+@app.route('/html')
+def quote():
+    with open('data/quotes.txt', 'r') as f:
+        quotes = f.readlines()
+    return render_template('index.html', quote=escape(get_quote()))
+
+@app.route('/plain')
+@app.route('/raw')
+@app.route('/txt')
+def metrics():
+    response = make_response(get_quote(), 200)
+    response.mimetype = "text/plain"
+    return response
+
+
+if __name__ == '__main__':
+   app.run()
--- cestpasle-generator/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Top-level package for C-est-pas-le Generator."""
-
-__author__ = """Ludovic Rivallain"""
-__version__ = '0.1.0'
-
-from .cestpasle_generator import *
