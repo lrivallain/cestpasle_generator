62b484ffb676543a4346f71d52130b1e9cb8ecee
Ludovic Rivallain <ludovic.rivallain@gmail.com>
2022-10-14 11:34:08 +0200

    Iconify

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Main module."""
 
 import random
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, send_from_directory
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -32,6 +32,10 @@ def metrics():
     response.mimetype = "text/plain"
     return response
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
    app.run()
